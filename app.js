4e120aaccd74f8e39c1268b9bb000605c4c819ec
viveloper <usik1234@gmail.com>
2020-02-27 08:50:33 +0900

    completed template version

--- app.js
+++ app.js
@@ -1,51 +1,254 @@
-const template = `
-    <form class="form" id="form">
-      <h2 class="form-title">Register With Us</h2>
-      <div class="form-control">
-        <label for="username">Username</label>
-        <input type="text" id="username" placeholder="Enter username" />
-        <small>Error message</small>
-      </div>
-      <div class="form-control">
-        <label for="email">Email</label>
-        <input type="text" id="email" placeholder="Enter email" />
-        <small>Error message</small>
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" placeholder="Enter password" />
-        <small>Error message</small>
-      </div>
-      <div class="form-control">
-        <label for="password2">Confirm Password</label>
-        <input
-          type="password"
-          id="password2"
-          placeholder="Enter password again"
-        />
-        <small>Error message</small>
-      </div>
-      <button type="submit" class="btn-submit">Submit</button>
-    </form>
-  `;
+class App {
+  constructor() {
+    this.state = {
+      username: {
+        key: 'username',
+        value: '',
+        errorMsg: ''
+      },
+      email: {
+        key: 'email',
+        value: '',
+        errorMsg: ''
+      },
+      password: {
+        key: 'password',
+        value: '',
+        errorMsg: ''
+      },
+      confirmPassword: {
+        key: 'confirmPassword',
+        value: '',
+        errorMsg: ''
+      },
+      focus: {
+        id: 'username',
+        position: 0
+      }
+    };
 
-const container = document.querySelector('#app');
+    this.el = document.querySelector('#app');
+  }
 
-const render = () => {
-  container.innerHTML = template;
-  bindEvents();
-};
-const bindEvents = () => {};
+  setState(newState) {
+    this.state = newState;
+    this.render();
+  }
 
-render();
+  bindEvents() {
+    const form = document.querySelector('#form');
+    const username = document.querySelector('#username');
+    const email = document.querySelector('#email');
+    const password = document.querySelector('#password');
+    const confirmPassword = document.querySelector('#confirmPassword');
+
+    form.addEventListener('submit', this.handleSubmit.bind(this));
+    username.addEventListener('keyup', this.handleInputChange.bind(this));
+    email.addEventListener('keyup', this.handleInputChange.bind(this));
+    password.addEventListener('keyup', this.handleInputChange.bind(this));
+    confirmPassword.addEventListener(
+      'keyup',
+      this.handleInputChange.bind(this)
+    );
+  }
+
+  handleInputChange(e) {
+    const newState = {
+      ...this.state,
+      [e.target.id]: {
+        ...this.state[e.target.id],
+        value: e.target.value
+      },
+      focus: {
+        id: e.target.id,
+        position: e.target.selectionStart
+      }
+    };
+    this.setState(newState);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.checkRequire([
+      this.state.username,
+      this.state.email,
+      this.state.password,
+      this.state.confirmPassword
+    ]);
+    this.checkLength(this.state.username, 3, 15);
+    this.checkLength(this.state.password, 6, 25);
+    this.checkEmail(this.state.email);
+    this.checkPasswordMatch(this.state.password, this.state.confirmPassword);
+
+    if (
+      this.state.username.errorMsg === 'success' &&
+      this.state.email.errorMsg === 'success' &&
+      this.state.password.errorMsg === 'success' &&
+      this.state.confirmPassword.errorMsg === 'success'
+    ) {
+      console.log('submit', {
+        username: this.state.username.value,
+        email: this.state.email.value,
+        password: this.state.password.value,
+        confirmPassword: this.state.confirmPassword.value
+      });
+    } else {
+      console.log('input is not valid');
+    }
+  }
+
+  checkRequire(inputList) {
+    inputList.forEach(input => {
+      if (input.value.trim().length === 0) {
+        this.setState({
+          ...this.state,
+          [input.key]: {
+            ...input,
+            errorMsg: `${input.key} is required`
+          }
+        });
+      } else {
+        this.setState({
+          ...this.state,
+          [input.key]: {
+            ...input,
+            errorMsg: `success`
+          }
+        });
+      }
+    });
+  }
+
+  checkLength(input, min, max) {
+    if (input.value.trim().length < min) {
+      this.setState({
+        ...this.state,
+        [input.key]: {
+          ...input,
+          errorMsg: `${input.key} must be at least ${min} characters`
+        }
+      });
+    } else if (input.value.trim().length > max) {
+      this.setState({
+        ...this.state,
+        [input.key]: {
+          ...input,
+          errorMsg: `${input.key} must be at least ${max} characters`
+        }
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        [input.key]: {
+          ...input,
+          errorMsg: `success`
+        }
+      });
+    }
+  }
+
+  checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(String(input.value.trim()).toLowerCase())) {
+      this.setState({
+        ...this.state,
+        [input.key]: {
+          ...input,
+          errorMsg: `success`
+        }
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        [input.key]: {
+          ...input,
+          errorMsg: `Email is not valid`
+        }
+      });
+    }
+  }
+
+  checkPasswordMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+      this.setState({
+        ...this.state,
+        [input2.key]: {
+          ...input2,
+          errorMsg: `Password do not match`
+        }
+      });
+    }
+  }
+
+  focusInput() {
+    const input = document.getElementById(this.state.focus.id);
+    input.focus();
+    input.selectionStart = this.state.focus.position;
+  }
+
+  render() {
+    const { username, email, password, confirmPassword } = this.state;
+
+    const getClassName = input => {
+      let className = '';
+      if (input.errorMsg === '') {
+        className = 'form-control';
+      } else if (input.errorMsg === 'success') {
+        className = 'form-control success';
+      } else {
+        className = 'form-control error';
+      }
+      return className;
+    };
+
+    const template = `
+      <form class="form" id="form">
+        <h2 class="form-title">Register With Us</h2>
+        <div class="${getClassName(username)}">
+          <label for="username">Username</label>
+          <input type="text" id="username" placeholder="Enter username" value="${
+            username.value
+          }" />
+          <small>${username.errorMsg}</small>
+        </div>
+        <div class="${getClassName(email)}">
+          <label for="email">Email</label>
+          <input type="text" id="email" placeholder="Enter email" value="${
+            email.value
+          }" />
+          <small>${email.errorMsg}</small>
+        </div>
+        <div class="${getClassName(password)}">
+          <label for="password">Password</label>
+          <input type="password" id="password" placeholder="Enter password" value="${
+            password.value
+          }" />
+          <small>${password.errorMsg}</small>
+        </div>
+        <div class="${getClassName(confirmPassword)}">
+          <label for="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder="Enter password again"
+            value="${confirmPassword.value}"
+          />
+          <small>${confirmPassword.errorMsg}</small>
+        </div>
+        <button type="submit" class="btn-submit">Submit</button>
+      </form>
+    `;
+    this.el.innerHTML = template;
+    this.bindEvents();
+    this.focusInput();
+  }
+}
+
+const app = new App();
+app.render();
 
 // ===========================================================================
-
-// const form = document.querySelector('#form');
-// const username = document.querySelector('#username');
-// const email = document.querySelector('#email');
-// const password = document.querySelector('#password');
-// const password2 = document.querySelector('#password2');
 
 // form.addEventListener('submit', e => {
 //   e.preventDefault();
